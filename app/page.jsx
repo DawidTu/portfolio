c3d0472cbbc8d086784e8f70f9d179529ff2ab2f
DavidTuma <dejvid.tuma@gmail.com>
2024-04-03 14:38:59 +0200

    new

--- app/page.jsx
+++ app/page.jsx
@@ -12,20 +12,25 @@ export default function Home() {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect( () => {
-    (
-      async () => {
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
-          const locomotiveScroll = new LocomotiveScroll();
-
-          setTimeout( () => {
-            setIsLoading(false);
-            document.body.style.cursor = 'default'
-            window.scrollTo(0,0);
-          }, 2000)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const LocomotiveScroll = (await import('locomotive-scroll')).default;
+        const locomotiveScroll = new LocomotiveScroll();
+  
+        setTimeout(() => {
+          setIsLoading(false);
+          document.body.style.cursor = 'default';
+          window.scrollTo(0, 0);
+        }, 2000);
+      } catch (error) {
+        console.error('Error loading locomotive-scroll:', error);
       }
-    )()
-  }, [])
+    };
+  
+    fetchData();
+  }, []);
+  
   
   return (
     <main>
